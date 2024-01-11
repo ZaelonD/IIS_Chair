6c4738ad9cd8d63dd721a7b39b727033a301aaa4
Александр Дубровин <xah799@mail.ru>
2024-01-11 10:29:58 +0300

    Implemented chooseOperatingParameter in the loop

--- src/IISLogicImpl.java
+++ src/IISLogicImpl.java
@@ -1,4 +1,5 @@
 import enums.MarketingTask;
+import enums.OperatingParameters;
 
 import java.util.Scanner;
 
@@ -8,6 +9,7 @@ public class IISLogicImpl {
 
     void start() {
         MarketingTask marketingTask = chooseAMarketingTask();
+        OperatingParameters operatingParameters = chooseOperatingParameter();
     }
 
     MarketingTask chooseAMarketingTask() {
@@ -20,6 +22,21 @@ public class IISLogicImpl {
                 choice = MarketingTask.BEST_PRODUCT;
             else if (answer == MarketingTask.values()[1].ordinal() + 1)
                 choice = MarketingTask.THE_MOST_ERGONOMIC_PRODUCT;
+            else
+                System.err.println("Данного варианта не существует! Попробуйте еще раз.\n");
+        }
+        return choice;
+    }
+
+    OperatingParameters chooseOperatingParameter() {
+        OperatingParameters choice = OperatingParameters.NULL_PARAMETER;
+        while (choice.equals(OperatingParameters.NULL_PARAMETER)) {
+            messages.printOperatingParametersMessage();
+            int answer = scanner.nextInt();
+            if (answer == OperatingParameters.values()[0].ordinal() + 1)
+                choice = OperatingParameters.OFFICE;
+            else if (answer == OperatingParameters.values()[1].ordinal() + 1)
+                choice = OperatingParameters.GAME;
             else
                 System.err.println("Данного варианта не существует! Попробуйте еще раз.\n");
         }

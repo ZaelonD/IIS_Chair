de1ad1ff7bdb59e5379dbffef6feb54b925a7b2e
Александр Дубровин <xah799@mail.ru>
2024-01-11 10:27:43 +0300

    Added printOperatingParametersMessage() method

--- src/Messages.java
+++ src/Messages.java
@@ -1,14 +1,22 @@
 import enums.MarketingTask;
+import enums.OperatingParameters;
 import enums.Sections;
 
 public class Messages {
     void printGeneralTitleMessage() {
-        System.out.println(Sections.GENERAL_TITLE.getSection());
+        System.out.println(Sections.GENERAL_TITLE.getDescription());
     }
     void printMarketingTaskMessage() {
-        System.out.println(Sections.SECTION1.getSection());
+        System.out.println(Sections.SECTION1.getDescription());
         System.out.println("1. " + MarketingTask.BEST_PRODUCT.getDescription());
         System.out.println("2. " + MarketingTask.THE_MOST_ERGONOMIC_PRODUCT.getDescription());
+        System.out.println("Ваш выбор: ");
+    }
+
+    void printOperatingParametersMessage() {
+        System.out.println(Sections.SECTION2.getDescription());
+        System.out.println("1. " + OperatingParameters.OFFICE.getDescription());
+        System.out.println("2. " + OperatingParameters.GAME.getDescription());
         System.out.println("Ваш выбор: ");
     }
 }

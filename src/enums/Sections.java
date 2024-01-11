52bf526745a11a6aede6c44788e1c233c26a9389
Александр Дубровин <xah799@mail.ru>
2024-01-11 10:27:05 +0300

    Created Sections enum

--- src/enums/Sections.java
+++ src/enums/Sections.java
@@ -4,19 +4,19 @@ public enum Sections {
     GENERAL_TITLE(Color.BLUE.getValue() + "Продукт, у которого будем определять оценку уровня " +
             "конкурентоспособности: \nкомпьютерное кресло Cougar Throne." + Color.RESET.getValue() + "\n"),
     SECTION1(Color.GREEN.getValue() + "Выберите маркетинговую задачу:" + Color.RESET.getValue()),
-    SECTION2("Выберите эксплуатационные параметры (тип кресла):"),
+    SECTION2(Color.GREEN.getValue() + "Выберите эксплуатационные параметры (тип кресла):" + Color.RESET.getValue()),
     SECTION3("Выберите технические параметры (тип подлокотников):"),
     SECTION4("Выберите эргономические параметры:"),
     SECTION5("Введите экономические параметры"),
     SECTION6("Введите надёжностные параметры");
 
-    private final String section;
+    private final String description;
 
-    Sections(String section) {
-        this.section = section;
+    Sections(String description) {
+        this.description = description;
     }
 
-    public String getSection() {
-        return section;
+    public String getDescription() {
+        return description;
     }
 }

b74f96f7fea6d76b516919fb6c37d4d90a21150e
Александр Дубровин <xah799@mail.ru>
2024-01-12 12:11:13 +0300

    Added Section for angle

--- src/enums/Sections.java
+++ src/enums/Sections.java
@@ -5,10 +5,11 @@ public enum Sections {
             "конкурентоспособности: \nкомпьютерное кресло Cougar Throne." + Color.RESET.getValue() + "\n"),
     SECTION1(Color.GREEN.getValue() + "Выберите маркетинговую задачу:" + Color.RESET.getValue()),
     SECTION2(Color.GREEN.getValue() + "Выберите эксплуатационные параметры (тип кресла):" + Color.RESET.getValue()),
-    SECTION3("Выберите технические параметры (тип подлокотников):"),
-    SECTION4("Выберите эргономические параметры:"),
-    SECTION5("Введите экономические параметры"),
-    SECTION6("Введите надёжностные параметры");
+    SECTION3(Color.GREEN.getValue() + "Выберите технические параметры (тип подлокотников):" + Color.RESET.getValue()),
+    SECTION4(Color.GREEN.getValue() + "Введите угол наклона спинки:" + Color.RESET.getValue()),
+    SECTION5("Выберите эргономические параметры:"),
+    SECTION6("Введите экономические параметры"),
+    SECTION7("Введите надёжностные параметры");
 
     private final String description;
 
